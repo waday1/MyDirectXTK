fb5e3e6a6c566f7533bc8ddfde2a206192dc27af
DESKTOP-BUUECPU\wada <g1527079@st.t-kougei.ac.jp>
2017-10-12 14:10:11 +0900

    猫

--- RabbitGameProject/RabbitGameProject/Game.h
+++ RabbitGameProject/RabbitGameProject/Game.h
@@ -6,7 +6,6 @@
 
 #include "StepTimer.h"
 
-
 // A basic game implementation that creates a D3D11 device and
 // provides a game loop.
 class Game
@@ -58,5 +57,10 @@ private:
     Microsoft::WRL::ComPtr<ID3D11DepthStencilView>  m_depthStencilView;
 
     // Rendering loop timer.
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_texture; 
+	std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
+	DirectX::SimpleMath::Vector2 m_screenPos;
+	DirectX::SimpleMath::Vector2 m_origin;
+
     DX::StepTimer                                   m_timer;
 };
